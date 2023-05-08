97e3282670b80464a58111842fd590237a92909f
Tamas Sulina <sulinat@gmail.com>
2023-05-08 14:30:16 +0100

    Initial commit

---
+++ UITestAutomation/SeleniumTests.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UITestAutomation
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestBbcUrl()
+        {
+            var options = new ChromeOptions();
+
+            var driver = new ChromeDriver();
+            var navigate = driver.Navigate();
+
+            var bbcScotlandUrl = "https://www.bbc.co.uk/scotland";
+            var bbcUrl = "https://www.bbc.co.uk/";
+
+            navigate.GoToUrl(bbcScotlandUrl);
+
+            driver.Url.Should().Be(bbcScotlandUrl, "URL is not BBC Scotland");
+            driver.FindElement(By.XPath("//button[text()='Yes, I agree']")).Click();
+            var b = driver.FindElements(By.CssSelector("a[href='https://www.bbc.co.uk']"));
+
+            b[0].Click();
+            driver.Url.Should().Be(bbcUrl, "URL is not BBC");
+        }
+    }
+}
